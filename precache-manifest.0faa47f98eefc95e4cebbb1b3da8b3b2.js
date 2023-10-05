b7a778b7af6c40ff143a339a8bd5d7b353a2f3f1
Cloud Hub <cloudcodehub@users.noreply.github.com>
2023-10-05 18:15:48 +0200

    Updates

---
+++ precache-manifest.0faa47f98eefc95e4cebbb1b3da8b3b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a24f9d6f085b7cafa99",
+    "url": "/my-app/static/css/main.65622938.chunk.css"
+  },
+  {
+    "revision": "1a24f9d6f085b7cafa99",
+    "url": "/my-app/static/js/main.839fffe3.chunk.js"
+  },
+  {
+    "revision": "a9e9488bf1c79412ffe2",
+    "url": "/my-app/static/js/runtime~main.ad3febe6.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/my-app/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/my-app/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "2f3ac82a1dbf3b0814790339ecf9b7fd",
+    "url": "/my-app/index.html"
+  }
+];
